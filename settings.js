6be2b47d0c285788912e77ca2214ee70e134f73e
Florian Hölken <hoelken@uni-wuppertal.de>
2025-08-27 06:39:40 +0200

    node red settings

--- settings.js
+++ settings.js
@@ -4,4 +4,12 @@ module.exports = {
   uiHost: env.NR_UI_HOST || "0.0.0.0",
   httpAdminRoot: env.NR_ADMIN_ROOT || "/",
   httpNodeRoot: env.NR_NODE_ROOT || "/",
+  adminAuth: {
+    type: "credentials",
+    users: [{
+      username: "solid-dataspace",
+      password: "$2y$08$.odZfecYCfsKeIZ.TNmQbes8hXU95PfjheTpYBs6EiMnEkPo5wz/O",
+      permissions: "*"
+    }]
+  }
 };
